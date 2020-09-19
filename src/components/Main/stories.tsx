cd4fbe1f0f62f7f40b6acb0fab674fc95bdb8756
Israel Carneiro <isragcarneiro@gmail.com>
2020-09-18 21:32:04 -0300

    chore: add .github/workflow to working with github actions

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -5,11 +5,15 @@ export default {
   title: 'Main',
   component: Main,
   args: {
-    title: 'default_title',
-    description: 'default_description'
+    title: 'title default',
+    description: 'description default'
   }
 } as Meta
 
 export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'title basic',
+  description: 'description basic'
+}
 
 export const Default: Story = (args) => <Main {...args} />
